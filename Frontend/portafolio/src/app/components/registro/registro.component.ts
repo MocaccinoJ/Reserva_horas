601da0656aa3bf37ab8a053c0f97966f21e30765
MocaccinoJ <68201449+MocaccinoJ@users.noreply.github.com>
2020-09-16 14:55:20 -0300

    Metodo de registro con errores

--- Frontend/portafolio/src/app/components/registro/registro.component.ts
+++ Frontend/portafolio/src/app/components/registro/registro.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { UsuariosService } from 'src/app/servicio/usuarios.service';
+import { UsuarioService } from 'src/app/servicio/usuario.service';
 import { Usuario } from 'src/app/interfaces/usuario';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-registro',
@@ -8,49 +9,60 @@ import { Usuario } from 'src/app/interfaces/usuario';
   styleUrls: ['./registro.component.css']
 })
 export class RegistroComponent implements OnInit {
- 
+  hasLower: boolean = false;
+  hasNum: boolean = true;
+  
+  constructor (private usuarioService: UsuarioService,
+    private router:Router) {}
 
-  constructor(private service: UsuariosService) { }
+  
 
-   usuario: Usuario = {
-   nombre:'',
-   correo:'',
-   contrasenha:''
-  }
+  
+  usuario: Usuario = {
+    nombre: '',
+    correo: '',
+    contrasenha: ''
+
+}
+
+
   ngOnInit(): void {
   }
 
-
   validarDatos(): void {
-   if (!this.usuario.correo){
-     alert("Ingrese un correo")
-     }
- }
+    if (!this.usuario.nombre){
+      alert("Ingrese un nombre")
+      return;
+    }
 
-   agregarUsuario(): void{
-   this.service.agregarUsuario(this.usuario).subscribe(_ => {alert("Registro exitoso");},
-   error => {alert("Este correo ya se encuentra en uso")}
-   );
- }
-
-}
-
-
-/*  
-
-  validarDatos(): void {
     if (!this.usuario.correo){
       alert("Ingrese un correo")
+      return;
     }
-  }
 
+    if (!this.usuario.contrasenha){
+      alert("Ingrese una contraseña")
+      return;
+    }
 
-  agregarUsuario(): void{
-    this.usuarioService.agregarUsuario(this.usuario).subscribe(_ => {alert("Registro exitoso");},
+    if(this.usuario.contrasenha.length < 4){
+      alert("La contraseña debe tener al menos 4 caracteres")
+      return;
+    }
+
+    if(this.usuario.contrasenha.search(/[a-zA-Z]/)== -1){
+      alert("La contraseña debe tener al menos una letra y un número");
+      return;
+    }
+
+    if(this.usuario.contrasenha.search(/\d/) == -1){
+      alert("La contraseña debe tener al menos una letra y un número");
+      return;
+    }
+
+    this.usuarioService.agregarUsuario(this.usuario).subscribe(_ => {alert("Registro exitoso"); this.router.navigate(['']);},
     error => {alert("Este correo ya se encuentra en uso")}
     );
-    
-  }
 
+  }
 }
- */
